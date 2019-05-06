464cda0fbfdc1779aaf5630d184598682467e07e
Pavel Svec <pavel.svec@itcrawl.cz>
2019-05-06 20:37:01 +0200

    Add project files.

---
+++ schedule/Schedule.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+using System.Globalization;
+
+namespace ScheduleNS
+{
+    public struct Interval
+    {
+        public DateTime Od, Do;
+
+        public Interval (DateTime od, TimeSpan trvani)
+        {
+            Od = od;
+            Do = od + trvani;
+        }
+    }
+
+    public struct Udalost
+    {
+        public int Id;
+        public Interval interval;
+
+        // int id|DateTime(Od)|DateTime(Do)
+        public static Udalost? UdalostZeStringu(string s)
+        {
+            Udalost u = new Udalost();
+            string[] rec = s.Split('|');
+            if (int.TryParse(rec[0], out u.Id) &&
+                DateTime.TryParse(rec[2]+ " " + rec[3], CultureInfo.CreateSpecificCulture("cs-CZ"), 
+                    DateTimeStyles.None, out u.interval.Od) &&
+                DateTime.TryParse(rec[4]+ " " + rec[5], CultureInfo.CreateSpecificCulture("cs-CZ"), 
+                    DateTimeStyles.None, out u.interval.Do))
+                return u;
+            else
+                return null;
+        }
+
+        public static int[] NactiUcastniky(string s)
+        {
+            string[] UcastniciStr = s.Split(',');
+            //{"8998", "5456", "5456"}
+            int[] Ucastnici = new int[UcastniciStr.Length];
+            //{8998, 5456, 5456}
+            int idx = 0;
+            foreach (string str in UcastniciStr)
+            {
+                if (int.TryParse(str, out Ucastnici[idx++]) == false)
+                    Console.WriteLine("Účastník {0} nerozeznán.", str);
+            }
+            return Ucastnici;
+        }
+
+        public static bool ZaznamJeUcastniku(string line, int[] ucastnikId)
+        {
+            int i = 0;
+            foreach (int id in ucastnikId)
+            {
+                if (int.TryParse(line.Split('|')[0], out i))
+                    if (i == id)
+                        return true;
+            }
+            return false;
+        }
+
+        public static Udalost?[] NactiUdalosti(int[] ucastnici, string[] udalostiStrArr)
+        {
+            Udalost?[] Udalosti = new Udalost?[udalostiStrArr.Length];
+
+            int idx = 0;
+            foreach (string s in udalostiStrArr)
+            {
+                if (ZaznamJeUcastniku(s, ucastnici))
+                {
+                    Udalost? u = Udalost.UdalostZeStringu(s);
+                    if (u != null)
+                        Udalosti[idx++] = u;
+                }
+            }
+            return Udalosti;
+        }
+    }
+
+    public class Schedule
+    {   
+        public static DateTime KonecPosledniSchuze(Udalost?[] uArr)
+        {
+            DateTime Nejvzdalenejsi = DateTime.Now;
+            foreach (Udalost u in uArr)
+                if (u.interval.Do > Nejvzdalenejsi)
+                    Nejvzdalenejsi = u.interval.Do;
+            return Nejvzdalenejsi;
+        }
+
+        public static Interval VolnyBlokV1Dnu(Udalost?[] udalosti, TimeSpan delkaVolnehoBloku,
+            DateTime start, DateTime konec)
+        {
+            bool[] blockedArray = new bool[(konec.Hour-start.Hour) * 60];
+            int startIndex = 0, endIndex = 0;
+            foreach (Udalost? u in udalosti)
+            {
+                if (u != null)
+                {
+                    if (u?.interval.Od.Hour <= start.Hour)
+                        startIndex = 0;
+                    else
+                        startIndex = (int)(u?.interval.Od - start)?.TotalMinutes;
+                    if (u?.interval.Do.Hour >= konec.Hour)
+                        endIndex = (konec.Hour - start.Hour) * 60 - 1;
+                    else
+                        endIndex = (int)(u?.interval.Do - start)?.TotalMinutes;
+                    for (int k = startIndex; k<endIndex && k < blockedArray.Length; k++)
+                        blockedArray[k] = true;
+                }
+            }
+
+            int volnyStart = 0, volnyKonec = 0;
+            int i = 0;
+            while (i < blockedArray.Length)
+            {
+                if(blockedArray[i] == false)
+                {
+                    volnyStart = i;
+                    while (blockedArray[i] == false && i < blockedArray.Length)
+                        i++;
+                    volnyKonec = i-1;
+                }
+               
+                if ((volnyKonec - volnyStart) >= delkaVolnehoBloku.TotalMinutes)
+                    return new Interval(start.AddMinutes(volnyStart), delkaVolnehoBloku);
+
+                volnyKonec = volnyStart;
+                i++;
+            }
+            return new Interval(konec, new TimeSpan(0));
+        }
+
+        static void Main(string[] args)
+        {
+            int[] ucastnici = Udalost.NactiUcastniky(Console.ReadLine());
+
+            string[] UdalostiStrArr = File.ReadAllLines("data.txt");
+            
+        }
+    }
+}
---
+++ unittestschedule1/UnitTest1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScheduleNS;
+
+namespace UnitTestSchedule1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        string[] sArr = {
+            "4227|Jan Novák|10.4.2019|8:00|10.4.2019|12:15\n",
+            "5899|Tomáš Jandásek|10.4.2019|8:30|10.4.2019|13:30\n",
+            "5899|Tomáš Jandásek|10.4.2019|14:45|10.4.2019|16:30\n",
+            "7889|Eva Jelínková|10.4.2019|7:00|10.4.2019|9:30\n",
+            "4227|Jan Novák|10.4.2019|15:30|10.4.2019|17:15\n",
+            "7889|Eva Jelínková|10.4.2019|14:35|10.4.2019|15:20\n"
+        };
+
+        string sUcastnici = "4227, 7889, 5899"; // volný termín od 13:30 do 14:30
+
+        [TestMethod]
+        public void TestVytvorUdalostZeStringu()
+        {
+            Udalost? u = Udalost.UdalostZeStringu("7889|Eva Jelínková|28.4.2019|14:35|10.4.2019|15:20");
+            Assert.IsNotNull(u);
+        }
+
+        [TestMethod]
+        public void TestVytvorPoleUdalosti()
+        {
+            int pocetNotNullPrvku = 0;
+
+            int[] ucastnici = Udalost.NactiUcastniky(sUcastnici);
+            Assert.IsTrue(ucastnici.Length == 3);
+
+            Udalost?[] u = Udalost.NactiUdalosti(ucastnici, sArr);
+            foreach (var v in u)
+                if (v != null) pocetNotNullPrvku++;
+            Assert.IsTrue(pocetNotNullPrvku == 6);
+        }
+
+        [TestMethod]
+        public void TestNajdiMozneTerminy()
+        {
+            int[] ucastnici = Udalost.NactiUcastniky(sUcastnici);
+
+            Udalost?[] u = Udalost.NactiUdalosti(ucastnici, sArr);
+
+            Interval volnyTermin = Schedule.VolnyBlokV1Dnu(u, new TimeSpan(1, 0, 0),
+            new DateTime(2019, 4, 10, 7, 0, 0), new DateTime(2019, 4, 10, 18, 0, 0));
+
+            Assert.IsTrue(volnyTermin.Od.Hour == 13 && volnyTermin.Od.Minute == 30
+                && volnyTermin.Do.Hour == 14 && volnyTermin.Do.Minute == 30);
+        }
+    }
+
+    [TestClass]
+    public class UnitTest2
+    {
+        string[] sArr = {
+            "4227|Jan Novák|10.4.2019|8:00|10.4.2019|12:15\n",
+            "5899|Tomáš Jandásek|10.4.2019|8:30|10.4.2019|13:30\n",
+            "5899|Tomáš Jandásek|10.4.2019|14:45|10.4.2019|16:30\n",
+            "7889|Eva Jelínková|10.4.2019|7:00|10.4.2019|9:30\n",
+            "4227|Jan Novák|10.4.2019|15:30|10.4.2019|17:15\n",
+            "7889|Eva Jelínková|10.4.2019|14:35|10.4.2019|15:20\n"
+        };
+
+        string sUcastnici = "4227, 7889"; // volný termín od 12:15 do 13:15
+
+        [TestMethod]
+        public void TestVytvorPoleUdalosti()
+        {
+            int pocetNotNullPrvku = 0;
+
+            int[] ucastnici = Udalost.NactiUcastniky(sUcastnici);
+            Assert.IsTrue(ucastnici.Length == 2);
+
+            Udalost?[] u = Udalost.NactiUdalosti(ucastnici, sArr);
+            foreach (var v in u)
+                if (v != null) pocetNotNullPrvku++;
+            Assert.IsTrue(pocetNotNullPrvku == 4);
+        }
+
+        [TestMethod]
+        public void TestNajdiMozneTerminy()
+        {
+            int[] ucastnici = Udalost.NactiUcastniky(sUcastnici);
+
+            Udalost?[] u = Udalost.NactiUdalosti(ucastnici, sArr);
+
+            Interval volnyTermin = Schedule.VolnyBlokV1Dnu(u, new TimeSpan(1, 0, 0),
+                new DateTime(2019, 4, 10, 7, 0, 0), 
+                new DateTime(2019, 4, 10, 18, 0, 0));
+
+            Assert.IsTrue(volnyTermin.Od.Hour == 12 && volnyTermin.Od.Minute == 15
+                && volnyTermin.Do.Hour == 13 && volnyTermin.Do.Minute == 15);
+        }
+    }
+}
